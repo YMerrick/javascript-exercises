37d82bf69b80b7c31cd927eb4b8ddb9ef69a90a1
York <yorkmerrick@outlook.com>
2024-07-01 15:43:10 +0100

    complete the fifth excercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (start < 0 || end < 0) return 'ERROR';
+    if (typeof start !== 'number') return 'ERROR';
+    if (typeof end !== 'number') return 'ERROR';
 
+    let sum = 0;
+
+    if (end < start) {
+        for (let i = end; i <= start; i++) {
+            sum += i;
+        }
+    } else {
+        for (let i = start; i <= end; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
