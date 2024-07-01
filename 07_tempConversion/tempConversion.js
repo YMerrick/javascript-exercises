f6d00a9f146a68b100ab71eb47eba35699f6f646
York <yorkmerrick@outlook.com>
2024-07-01 16:09:15 +0100

    complete the seventh exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+function round(num) {
+  return Math.round(num * 10) / 10;
+}
+
+const convertToCelsius = function(temperature) {
+  const convertedTemp = (temperature-32) * 5 / 9;
+  return round(convertedTemp);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  const convertedTemp = (temperature * 9 / 5) + 32;
+  return round(convertedTemp);
 };
 
 // Do not edit below this line
