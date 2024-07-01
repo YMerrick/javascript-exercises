364f2ab96a2cd4649dee9a8cee6afe373f0baee9
York <yorkmerrick@outlook.com>
2024-07-01 14:34:04 +0100

    complete the third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    if (word.length === 0) return word;
 
+    let output = '';
+
+    for (let i = word.length - 1; i >= 0; i--) {
+        output += word[i];
+    }
+    return output;
 };
 
 // Do not edit below this line
