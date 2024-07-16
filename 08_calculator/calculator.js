50375cfbe28d34ffccf47ba43b680d69d054e2c2
York <yorkmerrick@outlook.com>
2024-07-16 10:03:21 +0100

    complete the eighth exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	return nums.reduce(add,0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((total, current) => total * current,1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a <= 1) return 1;
+	return factorial(a-1) * a;
 };
 
 // Do not edit below this line
