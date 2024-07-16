48cdf970097f11c166ba6dcd88a6901e510f611c
York <yorkmerrick@outlook.com>
2024-07-16 11:54:38 +0100

    complete twelfth exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(list) {
+    return list.reduce( (oldest, current) => {
+        let oldestAge;
+        let currentAge;
 
+        if (!oldest.yearOfDeath) oldestAge = (new Date()).getFullYear() - oldest.yearOfBirth;
+        else oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+
+        if (!current.yearOfDeath) currentAge = (new Date()).getFullYear() - current.yearOfBirth;
+        else currentAge = current.yearOfDeath - current.yearOfBirth;
+        
+        return oldestAge > currentAge ? oldest: current;
+    });
 };
 
 // Do not edit below this line
