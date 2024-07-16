acca783c739debbaa98b11bf616689eb0bd342aa
York <yorkmerrick@outlook.com>
2024-07-16 11:18:09 +0100

    complete ninth exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    let stripped = input.replaceAll(/[^A-Za-z\d]/g,"").toLowerCase();
+    let start = 0;
+    let end = stripped.length - 1;
 
+    while (start < end) {
+        if (stripped[start] !== stripped[end]) return false;
+        start++;
+        end--;
+    }
+    return true;
 };
 
 // Do not edit below this line
